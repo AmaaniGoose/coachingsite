8ad53b005a24ccb432e6dd587dc88ae05052226b
AmaaniGoose <amanullah1707@gmail.com>
2020-08-19 15:38:39 +0530

    Added Footer, Header, Home and Main Components

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle} from 'reactstrap';
+
+
+function Home(props) {
+    return(
+        <div className="container">
+            <div className="row align-items-start">
+              <p>Beginning work on le Home Component</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
