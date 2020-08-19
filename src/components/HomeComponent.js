f53cc4e0fce96e5e12e71b719c3864e93ac6d037
AmaaniGoose <amanullah1707@gmail.com>
2020-08-19 16:25:56 +0530

    updated css and added carousel

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle} from 'reactstrap';
-
+    CardTitle, CardSubtitle, Container, Row, Col} from 'reactstrap';
+import  CarouselComponent  from './CarouselComponent'
 
 function Home(props) {
     return(
         <div className="container">
-            <div className="row align-items-start">
-              <p>Beginning work on le Home Component</p>
+            <div className="row justify-content-center">
+              <div className="col-12 col-md-12">
+            <CarouselComponent />
+            </div>
             </div>
         </div>
     );
