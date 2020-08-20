2bc9a905e15c224815bf667933673bad38473e21
AmaaniGoose <amanullah1707@gmail.com>
2020-08-20 18:00:17 +0530

    Updates

---
+++ precache-manifest.475c17438a06dff99e73051b66b01256.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480c70ebbb9026eb37b02d818f7fcd78",
+    "url": "/coachingsite/index.html"
+  },
+  {
+    "revision": "8b506d03c32b3ccf762d",
+    "url": "/coachingsite/static/css/2.9c130d88.chunk.css"
+  },
+  {
+    "revision": "c8de0ec3ef094e1e79bb",
+    "url": "/coachingsite/static/css/main.56840363.chunk.css"
+  },
+  {
+    "revision": "8b506d03c32b3ccf762d",
+    "url": "/coachingsite/static/js/2.8dd4319d.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/coachingsite/static/js/2.8dd4319d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8de0ec3ef094e1e79bb",
+    "url": "/coachingsite/static/js/main.f2287f99.chunk.js"
+  },
+  {
+    "revision": "7177ec47096dc34207d3",
+    "url": "/coachingsite/static/js/runtime-main.95502543.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/coachingsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/coachingsite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/coachingsite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/coachingsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/coachingsite/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
